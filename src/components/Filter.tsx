0911d52a27ab2f5525e8edd8d3f5b3efef8b1fb2
Finn Steimle <finnsteimle1@gmail.com>
2026-08-17 22:58:26 +0200

    style(filter): move add-filter button into ingredient list and simplify label

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,6 +1,8 @@
 import { useState, type Dispatch, type SetStateAction } from "react";
 import { FilterSelection } from "./FilterSelection";
 
+import styles from "./Filter.module.css";
+
 interface FilterProps {
   ingredientsToFilter: string[];
   setIngredientsToFilter: Dispatch<SetStateAction<string[]>>;
@@ -11,13 +13,17 @@ export function Filter({ ingredientsToFilter, setIngredientsToFilter }: FilterPr
   return (
     <div>
       <h3>Current ingredients you filter for:</h3>
-      <ul>
+      <ul className={styles.ingredientsToFilterList}>
         {ingredientsToFilter.map((i: string) => (
           <li key={i}>{i}</li>
         ))}
+        <li>
+          <button className={styles.filterSelectionButton} type="button" onClick={() => setSelectionMenuIsOpen(prev => !prev)}>+</button>
+        </li>
       </ul>
-      <button type="button" onClick={() => setSelectionMenuIsOpen(prev => !prev)}>Add more things to filter</button>
-      {selectionMenuIsOpen ? <FilterSelection ingredientsToFilter={ingredientsToFilter} setIngredientsToFilter={setIngredientsToFilter} /> : null }
+      <div className={styles.filterSelectionMenuContainer}>
+        {selectionMenuIsOpen ? <FilterSelection ingredientsToFilter={ingredientsToFilter} setIngredientsToFilter={setIngredientsToFilter} /> : null}
+      </div>
     </div>
   )
 }
