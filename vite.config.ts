20d8f609586780874a1bfd3ed3de5bcf363d1723
Finn Steimle <finnsteimle1@gmail.com>
2026-08-18 15:13:35 +0200

    refactor: trimmed out unnecessary things in configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
-// Import defineConfig helper from Vite for type-safe build configuration
 import { defineConfig } from 'vite';
-// Import Official React plugin for Vite to support JSX compilation & Fast Refresh
 import react from '@vitejs/plugin-react';
-// Import basic SSL plugin to generate self-signed HTTPS certificates for local development (required for iOS camera API access)
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
-// Export Vite configuration object
 export default defineConfig({
   plugins: [
-    react(), // Enable React JSX transformations
-    basicSsl() // Enable local HTTPS server mode for WebRTC/getUserMedia camera security requirements
+    react(),
+    basicSsl() // needed to enable local HTTPS server mode for WebRTC/getUserMedia camera security requirements
   ]
 });
-
-
