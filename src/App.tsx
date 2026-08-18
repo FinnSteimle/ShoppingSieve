ece5489ce8c0d53eed68b08123c365f6ce9c241b
Finn Steimle <finnsteimle1@gmail.com>
2026-08-18 11:37:26 +0200

    chore(app): remove legacy ingredient comment

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Filter } from './components/Filter/Filter';
 import { CodeScanner } from './components/CodeScanner';
 import { useState } from 'react';
 
-// const PRESET_AVOIDED_INGREDIENTS = ['Weizen', 'Roggen', 'Urdinkel', 'Hafer', 'Mais', 'Reis', 'Quinoa', 'Gerste', 'Hirse', "Dinkel"];
 
 function App() {
   const [ingredientsToFilter, setIngredientsToFilter] = useState<string[]>([]);
