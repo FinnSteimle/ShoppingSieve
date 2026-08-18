1ad3e2e7e422a68f5640df3231ea0897650d1dbc
Finn Steimle <finnsteimle1@gmail.com>
2026-08-18 19:35:48 +0200

    chore(filter, card): run npm run fix on Card/Filter

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,18 +2,14 @@ import type { ReactNode } from "react";
 
 import styles from "./Card.module.css";
 
-
 interface CardProps {
-  children: ReactNode
+  children: ReactNode;
 }
-
 
 export function Card({ children }: CardProps) {
   return (
     <div className={styles.outerContainer}>
-      <div className={styles.innerContainer}>
-        {children}
-      </div>
+      <div className={styles.innerContainer}>{children}</div>
     </div>
   );
 }
--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -12,15 +12,15 @@ interface FilterProps {
 export function Filter({ ingredientsToFilter, setIngredientsToFilter }: FilterProps) {
   return (
     <Card>
-        <h3>Current ingredients you filter for:</h3>
-        <ul className={styles.categoryList}>
-          {ingredientsToFilter.map((i: string) => (
-            <li key={i}>{i}</li>
-          ))}
-        </ul>
-        <CategoryList
-          ingredientsToFilter={ingredientsToFilter}
-          setIngredientsToFilter={setIngredientsToFilter}
+      <h3>Current ingredients you filter for:</h3>
+      <ul className={styles.categoryList}>
+        {ingredientsToFilter.map((i: string) => (
+          <li key={i}>{i}</li>
+        ))}
+      </ul>
+      <CategoryList
+        ingredientsToFilter={ingredientsToFilter}
+        setIngredientsToFilter={setIngredientsToFilter}
       />
     </Card>
   );
