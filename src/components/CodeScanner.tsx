7df2c58e14f34643e14740f67105e7e4745aaf95
Finn Steimle <finnsteimle1@gmail.com>
2026-08-18 22:55:54 +0200

    style(code-scanner): round scanner container and center its contents

--- src/components/CodeScanner.tsx
+++ src/components/CodeScanner.tsx
@@ -6,20 +6,22 @@ import styles from "./CodeScanner.module.css";
 export function CodeScanner() {
   return (
     <Card>
-      <h1>Scanner</h1>
-      {
-        <Scanner
-          classNames={{
-            container: styles.container,
-            video: styles.video,
-          }}
-          onScan={(result) => console.log(result)}
-          onError={(error) => console.log(error?.message)}
-          components={{
-            finder: false,
-          }}
-        />
-      }
+      <div className={styles.outerContainer}>
+        <h1>Scanner</h1>
+        {
+          <Scanner
+            classNames={{
+              container: styles.scannerContainer,
+              video: styles.video,
+            }}
+            onScan={(result) => console.log(result)}
+            onError={(error) => console.log(error?.message)}
+            components={{
+              finder: false,
+            }}
+          />
+        }
+      </div>
     </Card>
   );
 }
