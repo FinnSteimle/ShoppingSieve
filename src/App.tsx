8bda5901dde66be363068b350f2e52fe72070b74
Finn Steimle <finnsteimle1@gmail.com>
2026-08-18 19:13:57 +0200

    style(app): center the UI using flexbox spacers

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,24 @@ import { Filter } from "./components/Filter/Filter";
 import { CodeScanner } from "./components/CodeScanner";
 import { useState } from "react";
 
+import styles from "./App.module.css";
+
 function App() {
   const [ingredientsToFilter, setIngredientsToFilter] = useState<string[]>([]);
 
   return (
-    <>
-      <CodeScanner />
-      <Filter
-        ingredientsToFilter={ingredientsToFilter}
-        setIngredientsToFilter={setIngredientsToFilter}
-      />
-      <CurrentProductIngredients />
-    </>
+    <div className={styles.outerContainer}>
+      <div className={styles.placeholder}></div>
+      <div className={styles.componentContainer}>
+        <CodeScanner />
+        <Filter
+          ingredientsToFilter={ingredientsToFilter}
+          setIngredientsToFilter={setIngredientsToFilter}
+        />
+        <CurrentProductIngredients />
+      </div>
+      <div className={styles.placeholder}></div>
+    </div>
   );
 }
 
