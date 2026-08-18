ac25cf5f272cf4e14a1994b1be5291b6d68b03b7
Finn Steimle <finnsteimle1@gmail.com>
2026-08-18 19:14:24 +0200

    style(filter): style the filter card using the new tokens

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -10,17 +10,19 @@ interface FilterProps {
 
 export function Filter({ ingredientsToFilter, setIngredientsToFilter }: FilterProps) {
   return (
-    <>
-      <h3>Current ingredients you filter for:</h3>
-      <ul className={styles.categoryListContainer}>
-        {ingredientsToFilter.map((i: string) => (
-          <li key={i}>{i}</li>
-        ))}
-      </ul>
-      <CategoryList
-        ingredientsToFilter={ingredientsToFilter}
-        setIngredientsToFilter={setIngredientsToFilter}
-      />
-    </>
+    <div className={styles.outerContainer}>
+      <div className={styles.innerContainer}>
+        <h3>Current ingredients you filter for:</h3>
+        <ul className={styles.categoryList}>
+          {ingredientsToFilter.map((i: string) => (
+            <li key={i}>{i}</li>
+          ))}
+        </ul>
+        <CategoryList
+          ingredientsToFilter={ingredientsToFilter}
+          setIngredientsToFilter={setIngredientsToFilter}
+        />
+      </div>
+    </div>
   );
 }
